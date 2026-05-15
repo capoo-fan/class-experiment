707bb46d26302c64f89406770ac4f4828356c911
qianfu <qianfu46@gmail.com>
2026-05-16 00:09:59 +0800

    完成了数据结构lab3 和上传计组实验lab2

--- Data-Structure-and-algorithm /lab3/envelope.cpp
+++ Data-Structure-and-algorithm /lab3/envelope.cpp
@@ -8,9 +8,41 @@ typedef struct
     int height;
 } Envelope;
 
+int cmp(const void *a, const void *b)
+{
+    // 将 void 指针转换为 Envelope 指针
+    Envelope *e1 = (Envelope *)a;
+    Envelope *e2 = (Envelope *)b;
+
+    if (e1->width == e2->width)
+        return e2->height - e1->height;
+    return e1->width - e2->width;
+}
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 int maxEnvelopes(Envelope *envelopes, int n)
 {
-    // TODO
+    qsort(envelopes, n, sizeof(Envelope), cmp); // 找 h 的最长严格递增子序列
+    int ans = 0;
+    int *dp = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            int wi = envelopes[i].width, hi = envelopes[i].height;
+            int wj = envelopes[j].width, hj = envelopes[j].height;
+            if (hi > hj)
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+            ans = max(ans, dp[i]);
+        }
+    }
+    return ans;
 }
 
 int main()
